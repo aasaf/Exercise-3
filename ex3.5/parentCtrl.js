d800f81f7dc58c86a981a3b3a611f487336dc08e
aasaf <43063341+aasaf@users.noreply.github.com>
2019-07-23 16:48:12 +0300

    Add files via upload

---
+++ ex3.5/parentCtrl.js
@@ -0,0 +1,38 @@
+
+module.controller("parentCtrl", ParentCtrl)
+
+//let value1 
+
+// DI dependency injection - IOC
+function ParentCtrl($scope, $filter, $rootScope) {
+
+    editing = null;
+    var orderBy = $filter('orderBy');
+
+    $scope.devices = [
+        new Mobile({model: "IPhone X", color: "black", price: 3500}),
+        new Mobile({model: "Xiaomi", color: "green", price: 1000}),
+        new Mobile({model: "Samsung Galaxy 11+", color: "black", price: 5500}),
+        new Mobile({model: "LG V60", color: "White", price: 7500})
+    ]
+    
+    $scope.addMobile= function(){
+        let my_new_mobile = new Mobile({model:$scope.model, color:$scope.color, price:$scope.price})
+        $scope.devices.push(my_new_mobile);
+
+    }
+
+    $scope.removeMobile= function(index){
+      //  $scope.devices.splice(index,1);
+        $scope.devices.splice(index,1).deleteRow(index)
+       
+    }
+
+    $scope.order = function(predicate, reverse) {
+        $scope.devices = orderBy($scope.devices, predicate, reverse);
+      };
+
+    edit= function(m){
+        this.editing = m;
+      }
+}
